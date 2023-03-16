e9d9f626007b7f12591cdf35057842b132deee06
Rahul Verma <rv404674@gmail.com>
2023-03-16 17:10:49 +0530

    added the go commands

--- goCommands.go
+++ goCommands.go
@@ -4,14 +4,18 @@ func main() {
 
 	// BASIC GO COMMANDS
 	// NOTE: Most important ones
-	// go run - "
-	//go build - "compiles the program and creates an exec"
+	// go run - compiles the code and place it into a binary file in a temp folder, and then executes it.
+	// the binary file will be deleted once the execution is over.
+
+	// go build - creates a binary in the current working dir.
+	// go install - created a binary in $GOPATH/bin.
+	// NOTE: in >go 1.8, the $GOPATH defaults to your mac home.
+	// $GOPATH = $HOME
+
+	// go test - "run tests using files ending in _test.go"
+
 	//go doc - "prints documentation of the package"
 	//go fmt - "format source code files"
 	//go get - "download package and install them"
 	//go list - "install list of all packages"
-	//
-	//go run - "compiles and runs the exec"
-	//
-	//go test - "run tests using files ending in _test.go"
 }
